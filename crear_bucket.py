64deb6be4b44cf1eb05b9e9359d2d4c7a3de1e5a
EduardoAragon11 <166084429+EduardoAragon11@users.noreply.github.com>
2024-11-04 11:12:23 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -1,39 +1,20 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Entrada: Nombre del bucket desde el evento
+    # Entrada
     nombre_bucket = event['body']['bucket']
 
     # Inicializar el cliente S3
     client = boto3.client('s3')
 
-    # Proceso: Crear el bucket con ACL de acceso público
+    # Proceso: Crear el bucket
     response = client.create_bucket(
-        ACL='public-read-write',  # Permitir acceso público a leer y escribir
         Bucket=nombre_bucket,
+        GrantFullControl='id=634152101290',
         CreateBucketConfiguration={
-            'LocationConstraint': 'us-east-2'
-        }
-    )
-
-    # Crear una política de bucket para permitir acceso público a todos los objetos
-    public_policy = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "PublicReadGetObject",
-                "Effect": "Allow",
-                "Principal": "*",
-                "Action": "s3:GetObject",
-                "Resource": f"arn:aws:s3:::{nombre_bucket}/*"
-            }
-        ]
-    }
-
-    # Asignar la política pública al bucket
-    client.put_bucket_policy(
-        Bucket=nombre_bucket,
-        Policy=json.dumps(public_policy)
+            'LocationConstraint': 'us-east-1'
+        },
+        ObjectOwnership='BucketOwnerPreferred'
     )
 
     # Salida
