e948218625cd493b5df9f2a6347605afc407da5e
EduardoAragon11 <166084429+EduardoAragon11@users.noreply.github.com>
2024-11-08 21:52:52 -0500

    Update load_file_directorio_bucket.py

--- load_file_directorio_bucket.py
+++ load_file_directorio_bucket.py
@@ -1,30 +1,16 @@
+import json
 import boto3
+import base64
 
 def lambda_handler(event, context):
+    client = boto3.client("s3")
+    get_file_content = event["content"]
+    decode_content = base64.b64decode(get_file_content)
     bucket_name = "eaa-documentos-compras"
-    prefix = "probando/"
-    
-    # Initialize S3 client
-    s3_client = boto3.client('s3')
-    
-    # Create a file in /tmp
-    file_path = '/tmp/hello.txt'
-    try:
-        with open(file_path, 'w+') as file:
-            file.write('Hello, world!')
-        
-        # Upload the file to S3
-        s3_client.upload_file(file_path, bucket_name, prefix + 'hello-remote.txt')
-        
-        return {
-            'statusCode': 200,
-            'bucket': bucket_name,
-            'message': f"File uploaded successfully to {prefix}."
-        }
-    
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'error': str(e),
-            'message': "File upload failed."
-        }
+    s3_upload = client.put_object(Bucket=bucket_name,Key="data.pdf",Body=decode_content)
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('The Object is Uploaded successfully!')
+    }
