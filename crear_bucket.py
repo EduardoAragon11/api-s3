ed5843cfe44d3b8e35d5393f71cdcb23e63ecb31
EduardoAragon11 <166084429+EduardoAragon11@users.noreply.github.com>
2024-11-10 10:00:23 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -1,23 +1,29 @@
+import json
 import boto3
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-    # Entrada
-    nombre_bucket = event['body']['bucket']
-
-    # Inicializar el cliente S3
-    client = boto3.client('s3')
-
-    # Proceso: Crear el bucket
-    response = client.create_bucket(
-        Bucket=nombre_bucket,
-        CreateBucketConfiguration={
-            'LocationConstraint': 'us-east-1'
-        },
-        ObjectOwnership='BucketOwnerPreferred'
-    )
-
-    # Salida
-    return {
-        'statusCode': 200,
-        'response': response
-    }
+    # Initialize the S3 client
+    s3_client = boto3.client('s3')
+    
+    # Bucket name (unique across all AWS accounts)
+    bucket_name = 'eaa-new-bucket'  # Change to your desired bucket name
+    
+    # Define the AWS region
+    region = 'us-east-1'  # Change to your desired region
+    
+    try:
+        # Create the S3 bucket
+        response = s3_client.create_bucket(
+            Bucket=bucket_name
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f'Successfully created bucket: {bucket_name}')
+        }
+        
+    except ClientError as e:
+        return {
+            'statusCode': 501,
+            'body': json.dumps(f'Error: {e}')
+        }
